66fc5a3db6a8df36665e1ff363b57c09d7f5e99a
Manuel Calzolari <manuel.calzolari@gmail.com>
2019-02-26 21:00:19 +0100

    Improve setup files.

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
-setup(name='sklearn-genetic',
-      version='0.1',
-      description='Genetic feature selection module for scikit-learn',
-      url='http://github.com/manuel-calzolari/sklearn-genetic',
-      author='Manuel Calzolari',
-      author_email='',
-      license='LGPLv3',
-      install_requires=['scikit-learn>=0.18', 'deap>=1.0.2'],
-      packages=find_packages())
+setup(
+    name='sklearn-genetic',
+    version='0.1',
+    description='Genetic feature selection module for scikit-learn',
+    url='https://github.com/manuel-calzolari/sklearn-genetic',
+    author='Manuel Calzolari',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(),
+    python_requires='>=2.7',
+    install_requires=['scikit-learn>=0.18', 'deap>=1.0.2'],
+)
