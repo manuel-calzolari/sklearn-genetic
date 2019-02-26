b07f7521b068108546aa4983f70e9edca15e40f1
Manuel Calzolari <manuel.calzolari@gmail.com>
2019-02-26 22:24:21 +0100

    Add long description argument.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='sklearn-genetic',
     version='0.1',
     description='Genetic feature selection module for scikit-learn',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/manuel-calzolari/sklearn-genetic',
     download_url='https://github.com/manuel-calzolari/sklearn-genetic/archive/0.1.tar.gz',
     author='Manuel Calzolari',
