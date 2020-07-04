522d6d73cd7d7120f7fc057229e0e93dd9a10792
John Sandall <john@coefficient.ai>
2020-07-04 15:04:14 +0100

    Update version to 0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sklearn-genetic',
-    version='0.2',
+    version='0.3',
     description='Genetic feature selection module for scikit-learn',
     long_description=long_description,
     long_description_content_type='text/markdown',
